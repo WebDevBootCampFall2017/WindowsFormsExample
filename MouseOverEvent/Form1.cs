022c635fe8526f22b3be2fce78c3cdc98166154b
Jeremy <namingwaysway@gmail.com>
2017-08-06 19:18:21 -0700

    first commit

---
+++ MouseOverEvent/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseOverEvent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            this.button1.Text = "Don't you dare!";
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            this.button1.Text = "Click Me!";
+        }
+    }
+}
